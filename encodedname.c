e6757df1e08979b48ab0fa24fc116219a5f465b2
Rahul Dixit <125376372+hackpure@users.noreply.github.com>
2023-02-27 02:41:59 +0530

    Question 7

---
+++ encodedname.c
@@ -0,0 +1,10 @@
+// Encoded Name
+
+#include <stdio.h>
+
+int main() {
+    int W1, W2, W3, W4, W5;
+    scanf("%d&%d&%d&%d&%d", &W1, &W2, &W3, &W4, &W5);
+    printf("%c%c%c%c%c", W1, W2, W3, W4, W5);
+    return 0;
+}
