85f6c963b708fcab32f9c4f9a9258121ec205ff5
Rahul Dixit <125376372+hackpure@users.noreply.github.com>
2023-02-27 02:36:39 +0530

    Question 3

---
+++ wonderchoco.c
@@ -0,0 +1,12 @@
+// Wonderful Chocolate Offer
+
+#include <stdio.h>
+
+int main() {
+    int C, N, D;
+    scanf("%d", &C);        
+    scanf("%d", &N);
+    scanf("%d", &D);
+    printf("%d", (C + N * D) - (1 * N));
+    return 0;
+}
